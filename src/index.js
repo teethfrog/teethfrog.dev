048ec7b916122e3f184d139befa1b130ecdeda48
Alexander Kasianov <117091619+teethfrog@users.noreply.github.com>
2025-01-29 22:54:38 +0200

    skills section wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-"use strict";
+function copyToClipboard() {
+    const text = "kasianovalexander@outlook.com";
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Copied ' + text + ' to clipboard');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
